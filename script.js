f34e0b5c3bf460901e8dc6fafe02ff4bb6759c5c
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-10-08 11:24:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var sorteados = [];
+var valorMaximo = 500;
+
+function criarUnico(inicio, fim) {
+    var sugestao;
+
+    do {
+        sugestao = Math.ceil(Math.random() * valorMaximo);
+    } while (sugestao < inicio || sugestao > fim || sorteados.includes(sugestao));
+
+    sorteados.push(sugestao);
+    return sugestao;
+}
+
+var time = document.querySelector('.time');
+var sortearNovo = document.getElementById('sortearNovo');
+sortearNovo.addEventListener('click', function () {
+    var inicio = parseInt(document.getElementById('inicio').value) || 1;
+    var fim = parseInt(document.getElementById('fim').value) || valorMaximo;
+
+    if (inicio > fim) {
+        alert("O valor inicial deve ser menor ou igual ao valor final.");
+        return;
+    }
+
+    novoNumero.innerHTML = "";
+    time.style.display = "block";
+    novoNumero.className = "vencedor animando";
+    
+    setTimeout(function () {
+        novoNumero.className = "vencedor";
+    }, 3000);
+
+    // Adicionamos um intervalo para mostrar os números rapidamente antes de parar
+    var intervalo = 100;
+    var contador = 0;
+    var intervaloID = setInterval(function () {
+        novoNumero.innerHTML = criarUnico(inicio, fim);
+        contador += intervalo;
+        if (contador >= 3000) {
+            clearInterval(intervaloID);
+            time.style.display = "none";
+        }
+    }, intervalo);
+});
+
+var novoNumero = document.getElementById('novoNumero');
